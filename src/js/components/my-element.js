1d86a7fa33ce0b7f90fb41ff358e31036be14fb0
Ryan <ryangadinga90@gmail.com>
2025-07-29 10:12:20 +0700

    update my-element.js & add package-lock.json

--- src/js/components/my-element.js
+++ src/js/components/my-element.js
@@ -1,5 +1,26 @@
-import { LitElement } from 'lit';
+import { html, LitElement } from 'lit';
 
-class MyElement extends LitElement {}
+class MyElement extends LitElement {
+  static properties = {
+    name: {},
+  };
+
+  constructor() {
+    super();
+
+    this.name = 'John Doe';
+  }
+
+  render() {
+    return html`
+      <h1>Welcome, ${this.name}</h1>
+      <p>I hope you are in good condition</p>
+      <p>
+        Please visit <a href="https://www.dicoding.com/" target="_blank">This Site</a> to be
+        professional programmer
+      </p>
+    `;
+  }
+}
 
 customElements.define('my-element', MyElement);
