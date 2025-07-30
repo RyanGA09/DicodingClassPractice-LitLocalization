ee49b8224d21cb0b2fd2d5bf9576be241d5f3ed2
Ryan <ryangadinga90@gmail.com>
2025-07-30 10:50:36 +0700

    update js, html, package.json, & xliff

---
+++ src/generated/locales/es.js
@@ -0,0 +1,19 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    import {html} from 'lit';
+    import {str} from '@lit/localize';
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's85a488cacb57688b': str`Welcome, ${0}`,
+'sacac6b633604cb76': `I hope you are in good condition`,
+'hca0f4a7167f491bc': html`
+          Please visit <a href="https://www.dicoding.com/" target="_blank">This Site</a> to be a
+          professional programmer
+        `,
+    };
+  
